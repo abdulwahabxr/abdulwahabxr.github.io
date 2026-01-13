355d6d227aeed85c52b48e2591e0a0cfa1ba3342
abdulwahab <abdulwahab.xr@gmail.com>
2026-01-13 10:00:37 +0500

    -

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ const navMenu = document.getElementById("nav-menu");
 menuBtn.addEventListener("click", () => {
   navMenu.classList.toggle("show");
 });
+
+const faders = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+faders.forEach(el => observer.observe(el));
