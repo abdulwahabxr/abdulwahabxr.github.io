a2e8bd569bc5785d84a4fe19aaf579486327ece5
abdulwahab <abdulwahab.xr@gmail.com>
2026-01-13 09:41:35 +0500

    -

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Mobile menu toggle
+const menuBtn = document.getElementById("menu-btn");
+const navMenu = document.getElementById("nav-menu");
+
+menuBtn.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+});
